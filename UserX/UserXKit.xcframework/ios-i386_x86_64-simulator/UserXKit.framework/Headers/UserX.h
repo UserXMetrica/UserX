614bc3edf7581751fe61bc9cdd5d30766c3e38fe
UserX <userx@userx.pro>
2020-04-27 12:20:14 +0300

    Add UserXKit.xcframework

---
+++ UserX/UserXKit.xcframework/ios-i386_x86_64-simulator/UserXKit.framework/Headers/UserX.h
@@ -0,0 +1,19 @@
+//
+//  UserX.h
+//  UserX
+//
+//  Created by Andrey Korobkin on 07/06/2017.
+//  Copyright © 2017 JUST LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserX.
+FOUNDATION_EXPORT double UserXVersionNumber;
+
+//! Project version string for UserX.
+FOUNDATION_EXPORT const unsigned char UserXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserX/PublicHeader.h>
+
+
