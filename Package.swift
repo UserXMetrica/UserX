74626cc1c517f834f967115fa8d6f1afea7902a7
UserXMetrica <dev_reg@userx.pro>
2020-09-18 15:53:35 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,13 +11,14 @@ let package = Package(
     products: [
         .library(
             name: "UserXKit",
-            targets: ["UserXKit"]),
+            targets: ["UserXKit"])
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
             name: "UserXKit",
-            path: "UserX/UserXKit.xcframework"),
+            url: "https://github.com/UserXMetrica/UserX/releases/download/1.1.1/UserXKit.XCFramework_swift-5.3.zip",
+            checksum: "b56c924858c592cf50e192a23383f1325af70eb50b7244404b9559620232c902"
     ]
 )
