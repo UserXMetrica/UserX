1182336aea151f368da8d95f42858c5afb0b24b0
UserX <userx@userx.pro>
2020-11-26 14:03:36 +0300

    Improve crash reports and versioning.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UserXKit",
-            url: "https://github.com/UserXMetrica/UserX/releases/download/1.1.1/UserXKit.XCFramework_swift-5.3.zip",
-            checksum: "b56c924858c592cf50e192a23383f1325af70eb50b7244404b9559620232c902"
+            url: "https://github.com/UserXMetrica/UserX/releases/download/1.2.0/UserXKit.XCFramework_swift-5.3.zip",
+            checksum: "82b2118c569573cd8e5e07eb2ba08f9369c3546838a141049cd9231e20ae55b1"
     ]
 )
