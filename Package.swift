ce922fb8fb768e2fbcbf0a19de3cfcf39bac260f
UserXMetrica <dev_reg@userx.pro>
2021-02-12 11:55:55 +0300

    Update Package.swift
    Fix Package-file layout

--- Package.swift
+++ Package.swift
@@ -9,16 +9,12 @@ let package = Package(
         .iOS(.v8)
     ],
     products: [
-        .library(
-            name: "UserXKit",
-            targets: ["UserXKit"])
-    ],
-    dependencies: [
+        .library(name: "UserXKit",
+            	 targets: ["UserXKit"])
     ],
     targets: [
-        .binaryTarget(
-            name: "UserXKit",
-            url: "https://github.com/UserXMetrica/UserX/releases/download/1.4.0/UserXKit.XCFramework.zip",
-            checksum: "aa955a04d74bca259f4da2c247580e7a425ce2c9aa538ecdfe8f6a307af044f7"
+        .binaryTarget(name: "UserXKit",
+            	      url: "https://github.com/UserXMetrica/UserX/releases/download/1.4.0/UserXKit.XCFramework.zip",
+            	      checksum: "aa955a04d74bca259f4da2c247580e7a425ce2c9aa538ecdfe8f6a307af044f7")
     ]
 )
